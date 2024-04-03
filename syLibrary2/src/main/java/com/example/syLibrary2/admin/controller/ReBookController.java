c6ede4549d31456e0ac91132a43b0ae6929f6138
YW <dusdn5774@naver.com>
2024-04-03 17:22:40 +0900

    ReBook 오류수정

--- syLibrary2/src/main/java/com/example/syLibrary2/admin/controller/ReBookController.java
+++ syLibrary2/src/main/java/com/example/syLibrary2/admin/controller/ReBookController.java
@@ -1,10 +1,17 @@
 package com.example.syLibrary2.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.syLibrary2.admin.model.dao.ReBookDAO;
 
 @Controller
 @RequestMapping("/admin/*")
 public class ReBookController {
-
+	
+	@Autowired
+	ReBookDAO dao;
+	
+	
 }
