8202a6fb81164be559b6872751e70b8cbec40f49
Mihyunee <esther2187@naver.com>
2024-04-04 14:38:39 +0900

    아이디 찾기 기능 구현

--- syLibrary2/src/main/java/com/example/syLibrary2/user/model/dao/LoginDAO.java
+++ syLibrary2/src/main/java/com/example/syLibrary2/user/model/dao/LoginDAO.java
@@ -8,14 +8,15 @@ public interface LoginDAO {
 	String loginChk(String mId, String mpasswd);
 
 	// 아이디-이메일 찾기
-	String searchIdEmail(LoginDTO dto);
+	String searchIdEmail(String mEmail, String mName, String mBirthDate);
 
 	// 아이디-전화번호 찾기
-	String searchIdTel(LoginDTO dto);
+	String searchIdTel(String mTel, String mName, String mBirthDate);
 
 	// 비밀번호-이메일 찾기
 	String searchPwEmail(LoginDTO dto);
 
 	// 비밀번호-전화번호 찾기
 	String searchPwTel(LoginDTO dto);
+
 }
