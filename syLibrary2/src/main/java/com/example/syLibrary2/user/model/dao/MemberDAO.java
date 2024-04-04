63d239345827ffd2565dbd762f5521e23887a567
MY <didaldud5246@naver.com>
2024-04-04 16:48:28 +0900

    MEMBER 회원가입 관련 클래스 올립니당

---
+++ syLibrary2/src/main/java/com/example/syLibrary2/user/model/dao/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.example.syLibrary2.user.model.dao;
+
+import com.example.syLibrary2.user.model.dto.MemberDTO;
+
+public interface MemberDAO {
+     void insert_join(MemberDTO dto); //회원가입 
+	 MemberDTO detailMember(String m_Id); // 회원조회
+	 void edit_memberInfo(MemberDTO dto); // 회원정보 수정
+	 int checkId(String m_id);// 회원아이디 중복 체크
+	} 
+
+
--- syLibrary2/src/main/java/com/example/syLibrary2/user/model/dto/MemberDTO.java
+++ syLibrary2/src/main/java/com/example/syLibrary2/user/model/dto/MemberDTO.java
@@ -19,4 +19,28 @@ public class MemberDTO {
 	private String m_img;
 	private String m_year;
 	private int m_loan;
+	private int le_no;
+	private String le_name;
+	private int le_std;
+
+
+public MemberDTO() {}
+
+public MemberDTO(String m_name, String m_id, String m_birth_date, String m_tel, String m_email, String m_address, 
+		String m_img, String m_year, String m_zip_no, String m_detail_address,int le_no,String le_name,int le_std) {
+	super();
+	this.m_name = m_name;
+	this.m_id = m_id;
+	this.m_birth_date = m_birth_date;
+	this.m_tel = m_tel;
+	this.m_email = m_email;
+	this.m_address = m_address;
+	this.m_img = m_img;
+	this.m_year = m_year;
+	this.m_zip_no = m_zip_no; 
+	this.m_detail_address = m_detail_address;
+	this.le_no = le_no;
+	this.le_name = le_name;
+	this.le_std = le_std;
+ }
 }
