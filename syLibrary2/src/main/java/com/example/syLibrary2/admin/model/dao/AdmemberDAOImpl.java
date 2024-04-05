146a5b2c65399cf00fd321b7df5cf9b54ee13bb1
YW <dusdn5774@naver.com>
2024-04-05 15:42:40 +0900

    예약목록, 회원목록/상세 수정

--- syLibrary2/src/main/java/com/example/syLibrary2/admin/model/dao/AdmemberDAOImpl.java
+++ syLibrary2/src/main/java/com/example/syLibrary2/admin/model/dao/AdmemberDAOImpl.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.example.syLibrary2.admin.model.dto.LoanDTO;
 import com.example.syLibrary2.user.model.dto.MemberDTO;
 
 @Repository
@@ -17,44 +18,44 @@ public class AdmemberDAOImpl implements AdmemberDAO {
 	SqlSession sqlSession;
 	
 	@Override
-	public List<MemberDTO> ad_list_search(String search_option, String search, int start, int end) {
-		Map<String,Object> map=new HashMap<>();
-		map.put("search_option",search_option);
-		map.put("search",search);
-		map.put("start",start);
-		map.put("end",end);
-		return sqlSession.selectList("admember.ad_search_list",map);
+	public List<MemberDTO> list_search(String search_option, String search, int start, int end) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("search_option", search_option);
+		map.put("search", search);
+		map.put("start", start);
+		map.put("end", end);
+		return sqlSession.selectList("admember.list",map);
+	}
+	
+	@Override
+	public int count(String search_option, String search) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("search_option", search_option);
+		map.put("search", search);
+		return sqlSession.selectOne("admember.count", map);
 	}
 
 	@Override
 	public MemberDTO ad_list_detail(int m_no) {
 		return sqlSession.selectOne("admember.ad_detail",m_no);
 	}
-
+	
+	//-------회원대출상세
 	@Override
-	public int count() {
-		return sqlSession.selectOne("admember.ad_p_count");
-	}
-
-	@Override
-	public List<MemberDTO> list() {
-		return sqlSession.selectList("admember.ad_p_list");
-	}
-
-	@Override
-	public List<MemberDTO> list(int pageStart, int pageEnd) {
+	public List<LoanDTO> lo_memlist(int m_no) {
 		Map<String, Object> map = new HashMap<>();
-		map.put("start", pageStart);
-		map.put("end", pageEnd);
-		return sqlSession.selectList("admember.ad_p_list", map);
+		map.put("m_no",m_no);
+		return sqlSession.selectList("admember.lo_memlist", map);
 	}
 
 	@Override
-	public int search_count(String search_option, String search) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("search_option", search_option);
-		map.put("search", search);
-		return sqlSession.selectOne("admember.ad_search_count");
+	public LoanDTO loan_y(int m_no) {
+		return sqlSession.selectOne("admember.loan_y", m_no);
+	}
+
+	@Override
+	public LoanDTO reser_y(int m_no) {
+		return sqlSession.selectOne("admember.reser_y", m_no);
 	}
 
 }
